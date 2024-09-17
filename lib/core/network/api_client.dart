5b5d06421ce6700d228a10b3dc6d228bd4fa8712
İbrahim Halil Ulu <ibrahimhalilulu@tetrahgs.com.tr>
2024-09-17 09:10:57 +0300

    Network - api client

---
+++ lib/core/network/api_client.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'package:injectable/injectable.dart';
+
+import '../enum/request_type.dart';
+
+abstract class IApiClient {
+  Future<Response> fetch({
+    required String path,
+    Map<String, String>? headers,
+    Object? body,
+    RequestType requestType = RequestType.get,
+    bool useAuthorizationHeader = true,
+  });
+}
+
+@LazySingleton(as: IApiClient)
+final class ApiClient implements IApiClient {
+  final String _baseUrl = "https://example.com/api/";
+
+  late Map<String, String> _baseHeaders;
+
+  ApiClient() {
+    _baseHeaders = {
+      HttpHeaders.contentTypeHeader: 'application/json',
+    };
+  }
+
+  @override
+  Future<Response> fetch({
+    required String path,
+    Map<String, String>? headers,
+    Object? body,
+    RequestType requestType = RequestType.get,
+    bool useAuthorizationHeader = true,
+  }) async {
+    final Response response =
+        await _sendRequest(requestType, path, body, headers);
+    return response;
+  }
+
+  Future<Response> _sendRequest(
+    RequestType requestType,
+    String path,
+    Object? body,
+    Map<String, String>? headers,
+  ) {
+    final uri = _getUri(path);
+    final header = _header({..._baseHeaders, ...?headers});
+    final encodedBody = jsonEncode(body);
+
+    switch (requestType) {
+      case RequestType.post:
+        return http.post(uri, body: encodedBody, headers: header);
+      case RequestType.put:
+        return http.put(uri, body: encodedBody, headers: header);
+      case RequestType.patch:
+        return http.patch(uri, body: encodedBody, headers: header);
+      case RequestType.delete:
+        return http.delete(uri, body: encodedBody, headers: header);
+      case RequestType.get:
+        return http.get(uri, headers: header);
+    }
+  }
+
+  Uri _getUri(String path) {
+    return Uri.parse(_baseUrl + path);
+  }
+
+  Map<String, String> _header(Map<String, String>? headers) {
+    return {..._baseHeaders, ...?headers};
+  }
+}
